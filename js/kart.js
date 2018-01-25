32322071afe6c3b2fae94d6d92c2d7161e597dea
Ida Torgersen <idatorgersen@eduroam-nat-214.eduroam.uib.no>
2018-01-25 17:29:31 +0100

    Lagt til marker i kartet for studentsenteret og SV-fakultetet

--- js/kart.js
+++ js/kart.js
@@ -1,15 +1,27 @@
 function initMap() {
   var kvarteret = {lat: 60.389716, lng: 5.322079};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 14,
+    zoom: 15,
     center: kvarteret
   });
+  //Kvarteret
   var marker = new google.maps.Marker({
     position: kvarteret,
     map: map
   });
+  //Universitetsmuseet
   var marker2 = new google.maps.Marker({
     position: {lat: 60.387953, lng:5.321822},
+    map: map
+  });
+  //Studentsenteret
+  var marker3 = new google.maps.Marker({
+    position: {lat: 60.387953, lng:5.323139},
+    map: map
+  });
+  //SV
+  var marker4 = new google.maps.Marker({
+    position: {lat: 60.388549, lng:5.324381},
     map: map
   });
 }
