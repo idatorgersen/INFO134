9c019e51c621ca4befdc12fb11a66a483b2b7459
sindrekjr <sindre@sindrekjr.no>
2018-01-25 22:19:44 +0100

    Lagt til funksjonalitet for markers og skrevet litt om på kart.js: * Sortert data som brukes for markers inn i array "hotspots"; her kan enkelt ekstra informasjon legges inn * Metoden addMarker() kjøres for hvert elements som finnes i hotspots * Alle markers har nå en click-event som gjør at det dukker opp en info-boks med navnet på stedet markeren peker til (her kan det legges til mer info)

--- js/kart.js
+++ js/kart.js
@@ -1,27 +1,50 @@
+const hotspots = [
+  {
+    name: "Det Akademiske Kvarter",
+    pos: {lat: 60.389716, lng: 5.322079}
+  },
+  {
+    name: "Universitetsmuseet",
+    pos: {lat: 60.387953, lng:5.321822}
+  },
+  {
+    name: "Studentsenteret",
+    pos: {lat: 60.387953, lng:5.323139}
+  },
+  {
+    name: "SV-fakultetet",
+    pos: {lat: 60.388549, lng:5.324381}
+  }
+];
+
 function initMap() {
   var kvarteret = {lat: 60.389716, lng: 5.322079};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: kvarteret
   });
-  //Kvarteret
-  var marker = new google.maps.Marker({
-    position: kvarteret,
+
+  for(i in hotspots) addMarker(hotspots[i], map);
+}
+
+function addMarker(spot, map) {
+  let marker = new google.maps.Marker({
+    position: spot.pos,
+    title: spot.name,
     map: map
   });
-  //Universitetsmuseet
-  var marker2 = new google.maps.Marker({
-    position: {lat: 60.387953, lng:5.321822},
-    map: map
+
+  let info = new google.maps.InfoWindow({
+    content: spot.name
   });
-  //Studentsenteret
-  var marker3 = new google.maps.Marker({
-    position: {lat: 60.387953, lng:5.323139},
-    map: map
+
+  marker.addListener('click', function() {
+    info.open(map, marker);
   });
-  //SV
-  var marker4 = new google.maps.Marker({
-    position: {lat: 60.388549, lng:5.324381},
-    map: map
-  });
+
+  // Denne koden gjør at markers bouncer konstant etter onclick. Veldig unødvendig.
+  /*marker.addListener('click', function() {
+    if(this.getAnimation() !== null) this.setAnimation(null);
+    else this.setAnimation(google.maps.Animation.BOUNCE);
+  })*/
 }
