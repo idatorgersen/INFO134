964dab4b4fa8e068a52605f05403b47f0c58775e
Sindre S. Kjær <sindre@sindrekjr.no>
2018-02-26 16:40:26 +0100

    Opprettet js/toaletter.js, importert dennne filen+json i toaletter.html.

---
+++ js/toaletter.js
@@ -0,0 +1,40 @@
+
+function initMap() { // Google
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: getCenterCoordinates(toilets)
+  });
+}
+
+function addMarker(spot, map) { // Sindre
+  let info = new google.maps.InfoWindow({
+    content: spot.name
+  });
+
+  let marker = new google.maps.Marker({
+    position: spot.pos,
+    title: spot.name,
+    map: map,
+    infowindow: info
+  });
+
+  markers.push(marker);
+
+  marker.addListener('click', function() {
+    markers.forEach(function(marker) {
+      marker.infowindow.close(map, marker)
+    });
+    this.infowindow.open(map, marker);
+  });
+}
+
+function getCenterCoordinates(coordinates) { // Sindre
+  let lat = 0.0, lng = 0.0;
+  let ln = coordinates.length;
+  for(let i = 0; i < ln; i++) {
+    lat += coordinates[i].pos.lat;
+    lng += coordinates[i].pos.lng;
+  }
+
+  return {lat: lat / ln, lng: lng / ln};
+}
