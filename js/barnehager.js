159b1b7342b120d443ddfe237f37c631298f4063
Ida Torgersen <ida.torgersen@lyse.net>
2018-05-01 10:30:29 +0200

    Små endringer i barnehagesøk

--- js/barnehager.js
+++ js/barnehager.js
@@ -5,26 +5,31 @@ window.onload = function() {
     data = response;
   });
 }
- function eier(value) {
-   eier = value;
-   refresh();
+
+//Setter eier lik value og kjører refresh()
+function eier(value) {
+  eier = value;
+  refresh();
 }
 
+//Setter kapasitet lik value og kjører refresh()
 function maks(value){
   kapasitet = value;
   refresh();
 }
-
+//Filtrerer liste over barnehager ut fra hvilke radio buttons som er valgt
 function refresh(){
   var liste = data.filter(function(b){
     if(eier && b.eierform !== eier){
       return false;
     }
-    if (b.antallBarn > kapasitet){
+    if(kapasitet && b.antallBarn > kapasitet){
       return false;
+    }else if(b.antallBarn == undefined){
+      return null;
     }
     return true;
   });
-  console.log(liste[0])
+  console.log(liste);
   listPositions(liste);
 }
